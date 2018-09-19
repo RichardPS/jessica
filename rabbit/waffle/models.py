a0cae01d9f190fe12bd34e345469676f4094ccb0
Richard <richard.wood@primarysite.net>
2018-09-19 09:44:21 +0100

    inital waffle models created

--- rabbit/waffle/models.py
+++ rabbit/waffle/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    commenter = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    comment = models.TextField()
+    comment_date = models.DateTimeField(default=timezone.now)
