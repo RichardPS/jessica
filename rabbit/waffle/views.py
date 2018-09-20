38085ea72e02d7dfaacef0cd7ef48cffc880605d
Richard <richard.wood@primarysite.net>
2018-09-20 15:44:11 +0100

    First waffle view created

--- rabbit/waffle/views.py
+++ rabbit/waffle/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
+def posts(request):
+    posts = Post.objects.all().order_by('created_date')
+    return render(request, 'waffle/list_all_posts.html', {'posts': posts})
