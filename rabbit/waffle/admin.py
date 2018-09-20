0b2497bc8819c090da5e40d1b38e905f8159ae14
Richard <richard.wood@primarysite.net>
2018-09-20 11:50:24 +0100

    extending models

--- rabbit/waffle/admin.py
+++ rabbit/waffle/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Comment, Repost, Follow 
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Repost)
+admin.site.register(Follow)
--- rabbit/waffle/models.py
+++ rabbit/waffle/models.py
@@ -1,20 +1,58 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        )
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return '{0}: {1}'.format(self.author, self.id)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    commenter = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    commenter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        )
     comment = models.TextField()
     comment_date = models.DateTimeField(default=timezone.now)
 
+    def __str__(self):
+        return '{0}: {1}'.format(self.commenter, self.id)
+
 
 class Repost(models.Model):
-    reposter = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    reposter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+        )
     original_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='followers',
+        on_delete=models.CASCADE
+        )
+    followee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='followees',
+        on_delete=models.CASCADE
+        )
+
+
+class Mention(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name='mentions',
+        on_delete=models.CASCADE
+        )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
