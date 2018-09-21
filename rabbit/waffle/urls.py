2933d761e114a960126c1ce68d8f132c083f169f
Richard <richard.wood@primarysite.net>
2018-09-21 15:21:10 +0100

    Views extended

--- rabbit/waffle/urls.py
+++ rabbit/waffle/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+import re
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.posts, name='index'),
+    url(r'^$', views.posts, name='posts'),
+    url(r'^u/(?P<username>[\w]+)$', views.user_view, name='user_view'),
 ]
--- rabbit/waffle/views.py
+++ rabbit/waffle/views.py
@@ -6,3 +6,9 @@ from .models import Post
 def posts(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'waffle/list_all_posts.html', {'posts': posts})
+
+
+def user_view(request, username):
+    thisuser='richard'
+    posts = Post.objects.filter(author=username)
+    return render(request, 'waffle/list_all_posts.html', {'posts': posts})
