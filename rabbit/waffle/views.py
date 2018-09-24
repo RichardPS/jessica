2f964dd8c89869f55693e69dd9b7bd519c119d65
Richard <richard.wood@primarysite.net>
2018-09-24 16:08:23 +0100

    repost view expanded, not working currently

--- rabbit/waffle/views.py
+++ rabbit/waffle/views.py
@@ -1,14 +1,36 @@
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Post
+from .models import Repost
 
-# Create your views here.
+
 def posts(request):
     posts = Post.objects.all().order_by('created_date')
     return render(request, 'waffle/list_all_posts.html', {'posts': posts})
 
 
 def user_view(request, username):
-    print(username)
-    posts = Post.objects.filter(author__username=username).order_by('created_date')
-    return render(request, 'waffle/list_all_posts.html', {'posts': posts})
+    posts = Post.objects.filter(
+        author__username=username
+        ).order_by('created_date')
+
+    reposted = Repost.objects.filter(
+        reposter__username=username
+        )
+
+    post_list = []
+
+    for x in reposted:
+        post_list.append(x.original_post.pk)
+
+    print(post_list)
+
+    reposts = Post.objects.filter(
+        author__in=post_list
+        )
+
+    return render(
+        request,
+        'waffle/list_all_posts.html',
+        {'posts': posts},
+        {'reposts', reposts}
+        )
