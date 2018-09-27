548ef5b1d45bccbf1d7887eae558b82c2af754ca
Richard <richard.wood@primarysite.net>
2018-09-27 15:05:54 +0100

    fied view all posts view

--- rabbit/waffle/views.py
+++ rabbit/waffle/views.py
@@ -7,18 +7,30 @@ from .models import Repost
 
 
 def posts(request):
-    posts = Post.objects.all().order_by('created_date')
-    return render(request, 'waffle/list_all_posts.html', {'posts': posts})
+    allposts = Post.objects.all().order_by('created_date')
+    return render(
+        request,
+        'waffle/list_all_posts.html',
+        {'allposts': allposts}
+        )
 
 
 def user_view(request, username):
     posts = Post.objects.filter(
         author__username=username
-        ).annotate(is_repost=Value(0, models.IntegerField()))
+        ).annotate(
+        is_repost=Value(
+            0, models.IntegerField()
+            )
+        )
 
     reposted = Post.objects.filter(
         repost__reposter__username=username
-        ).annotate(is_repost=Value(1, models.IntegerField()))
+        ).annotate(
+        is_repost=Value(
+            1, models.IntegerField()
+            )
+        )
 
     allposts = posts.union(reposted).order_by('created_date')
 
